85385a9f6f57ddf022b722246b27529a0c7ebbf5
Anita's WorkBook <31773988+Anita7ITP@users.noreply.github.com>
2018-02-19 15:53:42 -0500

    Delete template.js

--- template.js
+++
@@ -1,7 +0,0 @@
-var data = {people: [{name: "Amanda", other: "Kate"}, {name: "Steven", other: "King"}{name: "Kim", other: "Klein"},{name: "Anita", other: "Ruth"},{name: "Judith", other: "Ben"},{name: "Neil", other: "Omarion"},]};
-
-
-app.get('/templatetest', function(req, res) {
-	var data = {person: {name: "Amanda", other: "Kate"}};
-    res.render('template.ejs', data);
-});
